690b6912833c92df6a5ba545f96c0d5436c7d6fa
Fernando <fmartinez@duobot.com.co>
2018-10-01 17:04:25 -0500

    Publicación vistas iniciales proyecto INLAB

--- src/main/java/com/example/duobot/inlab/controller/LoginController.java
+++ src/main/java/com/example/duobot/inlab/controller/LoginController.java
@@ -15,5 +15,55 @@ public class LoginController {
     public String welcome() {
         return "hello";
     }
+	
+	@RequestMapping(value = "/loginCliente")
+    public String loginCliente() {
+        return "loginCliente";
+    }
 
+	@RequestMapping(value = "/homeCliente")
+    public String homeCliente() {
+        return "homeCliente";
+    }
+	
+	@RequestMapping(value = "/video")
+    public String video() {
+        return "video";
+    }
+	
+	@RequestMapping(value = "/mediciones")
+    public String mediciones() {
+        return "mediciones";
+    }
+	
+	@RequestMapping(value = "/insight")
+    public String insight() {
+        return "insight";
+    }
+	
+	@RequestMapping(value = "/social")
+    public String social() {
+        return "social";
+    }
+	
+	@RequestMapping(value = "/galeria")
+    public String galeria() {
+        return "galeria";
+    }
+	
+	@RequestMapping(value = "/bi")
+    public String bi() {
+        return "bi";
+    }
+	
+	@RequestMapping(value = "/emociones")
+    public String emociones() {
+        return "emociones";
+    }
+	
+	@RequestMapping(value = "/homeAdmin")
+    public String homeAdmin() {
+        return "homeAdmin";
+    }
+	
 }
