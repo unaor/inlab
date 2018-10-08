fec868c9a25b2a2d0bf4b98e2b02c7de35b23453
u_naor@yahoo.com <urin@wise-mon.com>
2018-10-08 17:11:30 -0500

    fix in poll controller

--- src/main/java/com/example/duobot/inlab/controller/PollController.java
+++ src/main/java/com/example/duobot/inlab/controller/PollController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.duobot.inlab.dao.PollService;
 import com.example.duobot.inlab.model.Poll;
 
+@RestController
 public class PollController {
 	
 	@Autowired
