f0c26ef4f1d3536f75b9a22a658b4b4848c754f2
fernmarr <fmartinez@duobot.com.co>
2018-10-09 22:45:53 -0500

    actualizacion  post encuestas

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,82 +1,90 @@
-/*
- * Archivo de funciones para la vista /Admin
- */
+/* Archivo de funciones para la vista /Admin */
 
 /* GET JSON */
 
-$(document)
-		.ready(
-				function() {
+$(document).ready(function() {
+	
+	jQuery.support.cors = true;
 
-					jQuery.support.cors = true;
+	$.ajax({
+			type : "GET",
+			dataType : "json",
+			url : "/api/user",
+			success : function(data) {
 
-					$
-							.ajax({
-								type : "GET",
-								dataType : "json",
-								url : "/api/user",
-								success : function(data) {
+				console.log(data);
 
-									console.log(data);
+				var trHTML = '';
+				$.each(
+					data,
+					function(i, item) {
 
-									var trHTML = '';
+								// alert(item.enabled);
+								// alert(item.username);
 
-									$
-											.each(
-													data,
-													function(i, item) {
+						trHTML += '<tr><td>'
+								+ item.username
+								+ '</td><td>'
+								+ item.enabled
+								+ '</td><td><a class="btn btn-info btn-icon btn-sm " href="#" data-toggle="modal" data-target="#modalUsuario" data-tooltip="tooltip" data-placement="right" title="Editar" data-original-title="Editar"><i class="far fa-edit"></i></a> <button type="button" rel="tooltip" data-toggle="modal" data-target="#modalBorrar" class="btn btn-danger btn-icon btn-sm " data-original-title="Borrar" title="Borrar"><i class="far fa-trash-alt"></i></button></td></tr>';
+					});
 
-														// alert(item.enabled);
-														// alert(item.username);
+				$('#tbUsuarios').append(trHTML);
 
-														trHTML += '<tr><td>'
-																+ item.username
-																+ '</td><td>'
-																+ item.enabled
-																+ '</td><td><a class="btn btn-info btn-icon btn-sm " href="#" data-toggle="modal" data-target="#modalUsuario" data-tooltip="tooltip" data-placement="right" title="Editar" data-original-title="Editar"><i class="far fa-edit"></i></a> <button type="button" rel="tooltip" data-toggle="modal" data-target="#modalBorrar" class="btn btn-danger btn-icon btn-sm " data-original-title="Borrar" title="Borrar"><i class="far fa-trash-alt"></i></button></td></tr>';
-													});
+			},
 
-									$('#tbUsuarios').append(trHTML);
+			error : function(msg) {
+			alert(msg.responseText);
 
-								},
+			}
+		});
 
-								error : function(msg) {
 
-									alert(msg.responseText);
-								}
-							});
-				})
 
+				
 /* POST JSON ENCUESTAS */
 
-$(document).ready(function() {
-	// click on button submit
-	$("#btnPoll").on('click', function() {
-		var data = {};
-		$("#formPoll").serializeArray().map(function(x){data[x.name] = x.value;}); 
-		// send ajax
-		$.ajax({
-			url : '/api/poll', // url where to submit the request
-			type : "POST", // type of action POST || GET
-			dataType : 'json', // data type
-			contentType : 'application/json;charset=utf-8', // data type
-			data : JSON.stringify(data), // post data || get data
-			success : function(result) {
-				// you can see the result from the console
-				// tab of the developer tools
-				console.log(result);
-			},
+$("#btnPoll").on('click', function() {
+	
+	var vpollName = $("#pollName").val();
+	
+	var v1 = $("#startDate").val();
+	var v1format = moment(v1, "DD/MM/YYYY").valueOf() / 1000;
+	var v2 = $("#endDate").val();
+    var v2format = moment(v2, "DD/MM/YYYY").valueOf() / 1000;
+    
+    var data = {
+    		pollName: vpollName,
+    		startDate: v1format,
+    		endDate: v2format
+        };
+	
+	
+	console.log(data);
+
+	$.ajax({
+		url : '/api/poll',
+		type : "POST",
+		contentType : 'application/json',
+		data : data,
+		success : function(result) {
+			console.log(result);
+		},
 			error : function(xhr, resp, text) {
-				console.log(xhr, resp, text);
-			}
-		})
-	});
+			console.log(xhr, resp, text);
+			data: data
+		}
+	})
 });
 
+
 /* BORRAR FORMLARIOS */
-$(document).ready(function() {
-	$("#btnCerrar").click(function() {
-		/* Single line Reset function executes on click of Reset Button */
-		$("#formPoll")[0].reset();
-	});
+
+$("#btnCerrar").click(function() {
+	/* Single line Reset function executes on click of Reset Button */
+	$("#formPoll")[0].reset();
 });
+
+
+
+})
