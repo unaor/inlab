f6fbe38263213abd95ab168886041cbd918f954b
u_naor@yahoo.com <urin@wise-mon.com>
2018-10-23 08:27:59 -0500

    Added war build

--- src/main/java/com/example/duobot/inlab/InlabApplication.java
+++ src/main/java/com/example/duobot/inlab/InlabApplication.java
@@ -2,9 +2,10 @@ package com.example.duobot.inlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InlabApplication {
+public class InlabApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InlabApplication.class, args);
