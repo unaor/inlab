022171475c293de47e892d04dc5b3a264d0e1b57
u_naor@yahoo.com <urin@wise-mon.com>
2018-10-23 21:28:38 -0500

    added jar

--- src/main/java/com/example/duobot/inlab/InlabApplication.java
+++ src/main/java/com/example/duobot/inlab/InlabApplication.java
@@ -2,10 +2,9 @@ package com.example.duobot.inlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InlabApplication extends SpringBootServletInitializer {
+public class InlabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InlabApplication.class, args);
