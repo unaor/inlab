4ef690f9a4124bcb9bc1678a4d0634124191b90c
fernmarr <fmartinez@duobot.com.co>
2018-10-26 00:41:54 -0500

    Actualizar vista insight

--- src/main/resources/static/js/insight.js
+++ src/main/resources/static/js/insight.js
@@ -1,76 +1,124 @@
+/* GET JSON ENCUESTAS */
 
-//
-//$(document).ready(function() {
-//	
-//
-///* GET JSON RESPUESTAS PARA INSIGHT */
-//
-//		var vinsight = $("#hiddenInsightId").val();
-//		var url= "/api/answer?pollId="+vinsight;
-//		$.ajax({
-//			type : "GET",
-//			dataType : "json",
-//			url : url,
-//			success : function(data) {
-//			
-//				var tagsbox = '';
-//				var f = 1;
-//
-//				
-//
-//					$('#tagCloud').jQCloud(data.tags);
-//				
-//				
-//				
-//				
-//				
-//				
-////				$.each(
-////						data,
-////						function(i, item) {
-////							var VquestionName = item.questionName;
-////							console.log(VquestionName);
-////							
-////							var Vtags = item.tags;
-////							console.log(Vtags);
-////							
-////							tagsbox += '<h2>' + VquestionName + '</h2><br><div class="col-md-10 col-centered justify-content-center demo'+ f +' jqcloud"></div><br><hr>';
-////							
-////							$(function() {
-////								$('#demo'+ f +'').jqcloud(Vtags);
-////						      });
-////
-////							f++;
-////							
-////						});
-////				
-////					$('#tagCloud').append(tagsbox);
-//
-//			},
-//
-//		});
-//		
-//})
-//
-//
-////tagCloudHome
 
-//$("#btnGenerateCloud").click(() => {
-//
-//	var words = [
-//		  {word: "Lorem", weight: 13},
-//		  {word: "Ipsum", weight: 10.5},
-//		  {word: "Dolor", weight: 9.4},
-//		  {word: "Sit", weight: 8},
-//		  {word: "Amet", weight: 6.2},
-//		  {word: "Consectetur", weight: 5},
-//		  {word: "Adipiscing", weight: 5},
-//		];
-//
-//		$('#wordcloud').jQCloud(words, {
-//			width:500,
-//			height:350
-//		});
+	pollId = 
+
+	$.ajax({
+			type : "GET",
+			dataType : "json",
+			url : "/api/poll",
+			success : function(data) {
+				
+				
+				//const selectedPoll = data.filter(x => x.pollId === pollId)[0];
+				//console.log(selectedPoll);
+				
+				//var insightId = selectedPoll.insightId
+				//alert(insightId);
+					
+					
+					
+				
+
+
+				
+			},
+
+		});
 	
-//});
+	
+	
+	$.ajax({
+		type : "GET",
+		dataType : "json",
+		url : "/api/word",
+		success : function(data) {
 
+		
+
+		},
+
+	});
+	
+	
+	
+	
+
+
+
+
+
+
+var text_string = "extragrande gigante extragrande azul gigante rojo gigante de gigante color extragrande gigante marron extragrande naranja azul verde amarillo mediano rojo grande palabra1 mediano grande palabra2 palabra3 palabra3 pequeño extragrande mediano grande extragrande ";
+
+      drawWordCloud(text_string);
+
+      function drawWordCloud(text_string){
+        var common = "";
+
+        var word_count = {};
+
+        var words = text_string.split(/[ '\-\(\)\*":;\[\]|{},.!?]+/);
+          if (words.length == 1){
+            word_count[words[0]] = 1;
+          } else {
+            words.forEach(function(word){
+              var word = word.toLowerCase();
+              if (word != "" && common.indexOf(word)==-1 && word.length>1){
+                if (word_count[word]){
+                  word_count[word]++;
+                } else {
+                  word_count[word] = 1;
+                }
+              }
+              
+            })
+          }
+
+        var svg_location = "#cloudContainer";
+        var width = ($(document).width())/2;
+        var height = ($(document).height())/2;
+
+        var fill = d3.scale.category20();
+
+        var word_entries = d3.entries(word_count);
+        console.log(word_entries);
+
+        var xScale = d3.scale.linear()
+           .domain([0, d3.max(word_entries, function(d) {
+              return d.value;
+            })
+           ])
+           .range([1,100]);
+
+        d3.layout.cloud().size([width, height])
+          .timeInterval(20)
+          .words(word_entries)
+          .fontSize(function(d) { return xScale(+d.value); })
+          .text(function(d) { return d.key; })
+          .rotate(function() { return ~~(Math.random() * 2) * 90; })
+          .font("Impact")
+          .on("end", draw)
+          .start();
+
+        function draw(words) {
+          d3.select(svg_location).append("svg")
+              .attr("width", width)
+              .attr("height", height)
+            .append("g")
+              .attr("transform", "translate(" + [width >> 1, height >> 1] + ")")
+            .selectAll("text")
+              .data(words)
+            .enter().append("text")
+              .style("font-size", function(d) { return xScale(d.value) + "px"; })
+              .style("font-family", "Impact")
+              .style("fill", function(d, i) { return fill(i); })
+              .attr("text-anchor", "middle")
+              .attr("transform", function(d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+              })
+              .text(function(d) { return d.key; });
+        }
+
+        d3.layout.cloud().stop();
+      }
