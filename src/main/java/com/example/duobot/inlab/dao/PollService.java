f020c1156cb7eb7e668fb1cfe33ec3e487ecc957
u_naor@yahoo.com <urin@wise-mon.com>
2018-12-03 21:53:43 -0500

    fix answers

--- src/main/java/com/example/duobot/inlab/dao/PollService.java
+++ src/main/java/com/example/duobot/inlab/dao/PollService.java
@@ -1,9 +1,13 @@
 package com.example.duobot.inlab.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.duobot.inlab.model.Poll;
 
 public interface PollService extends CrudRepository<Poll, Integer> {
+	
+	public List<Poll> findAllByOrderByPollIdAscQuestions(); // shows multiple rows
 
 }
